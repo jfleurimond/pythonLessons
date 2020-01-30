6cd9c2c7d607dc212803fe4235c13197be38b6fc
juicehustlin <jfleurimond24@gmail.com>
2020-01-29 19:58:36 -0500

    exercise for user input

---
+++ ex11.py
@@ -0,0 +1,11 @@
+# exercise the shows how you recieve input from users and then print it back
+
+print("how are old are you?" , end=' ')
+age = input()
+# We put a end=' ' at the end of each print line. This tells print to not end the line with a newline character and go to the next line.
+print("How tall are you?" , end=' ')
+height = input()
+print("How much do you weigh", end=' ')
+weight = input()
+
+print(f"So, you're {age} years old, {height} tall and {weight} heavy.")
