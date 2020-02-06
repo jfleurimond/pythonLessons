fe32b7d311f267950ecf586fd5520b8fd0d5364f
juicehustlin <jfleurimond24@gmail.com>
2020-02-05 19:07:27 -0500

    exercise 17

--- ex14.py
+++ ex14.py
@@ -21,3 +21,5 @@ Alright, so you said {likes} about liking me.
 You live in {lives}. Not sure where that is.
 And you have a {computer} computer. Nice
 """)
+
+docker run -it --rm -p 8888:8888 -v /home/aandrews/Notebooks:/home/jovyan jupyter/datascience-notebook
